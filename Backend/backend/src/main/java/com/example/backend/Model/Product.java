c6c55669d5338c088bf9ca82ec3d182e0c2cc983
Gimhani Fernando <gimhanif44@gmail.com>
2023-03-27 14:36:24 +0530

    jdbc connection established. Product model class created

---
+++ Backend/backend/src/main/java/com/example/backend/Model/Product.java
@@ -0,0 +1,83 @@
+package com.example.backend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+public class Product {
+
+    private int product_id;
+    private String product_name;
+    private double product_price;
+    private String product_description;
+
+    private boolean status; //Stores whether product is in stock or not
+    private int product_quantity;
+    private boolean live;
+
+    private String product_imageName;
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public double getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(double product_price) {
+        this.product_price = product_price;
+    }
+
+    public String getProduct_description() {
+        return product_description;
+    }
+
+    public void setProduct_description(String product_description) {
+        this.product_description = product_description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public int getProduct_quantity() {
+        return product_quantity;
+    }
+
+    public void setProduct_quantity(int product_quantity) {
+        this.product_quantity = product_quantity;
+    }
+
+    public boolean isLive() {
+        return live;
+    }
+
+    public void setLive(boolean live) {
+        this.live = live;
+    }
+
+    public String getProduct_imageName() {
+        return product_imageName;
+    }
+
+    public void setProduct_imageName(String product_imageName) {
+        this.product_imageName = product_imageName;
+    }
+}
