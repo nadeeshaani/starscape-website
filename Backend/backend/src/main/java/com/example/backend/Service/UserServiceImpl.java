a93c5686fc7b39ba5db472bc0d2601ab384ef458
lathikaarosha <lathikaarosha123@gmail.com>
2023-03-28 19:27:16 +0530

    User API created

--- Backend/backend/src/main/java/com/example/backend/Service/UserServiceImpl.java
+++ Backend/backend/src/main/java/com/example/backend/Service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.backend.Service;
 
+import com.example.backend.Exception.ResourceNotFoundException;
 import com.example.backend.Model.User;
 import com.example.backend.Payload.UserDTO;
 import com.example.backend.Repository.UserRepository;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -19,21 +21,34 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDTO addUser(UserDTO userDTO) {
-        return null;
+        //userDTo to User conversion
+        User user = this.mapper.map(userDTO, User.class);
+
+        //saving..
+        User save = this.userRepository.save(user);
+
+        //User to UserDTO
+        UserDTO saveDTo = this.mapper.map(save, UserDTO.class);
+        return saveDTo;
     }
 
     @Override
     public UserDTO getUserById(int user_id) {
-        return null;
+        User userById = this.userRepository.findById(user_id).orElseThrow(()->new ResourceNotFoundException("User does not exist"));
+        UserDTO userByIdDTO = this.mapper.map(userById, UserDTO.class);
+        return userByIdDTO;
     }
 
     @Override
     public void deleteUser(int user_id) {
-
+        User user = this.userRepository.findById(user_id).orElseThrow(()->new ResourceNotFoundException("User does not exist"));
+        this.userRepository.delete(user);
     }
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return null;
+        List<User> allUsers = this.userRepository.findAll();
+        List<UserDTO> allUserDtos = allUsers.stream().map(each->this.mapper.map(each, UserDTO.class)).collect(Collectors.toList());
+        return allUserDtos;
     }
 }
