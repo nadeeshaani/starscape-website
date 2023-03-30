b713f4eaab30678b0357922707de1c9f0aa8ac1c
Nadeeshani <nawarat-se19043@stu.kln.ac.lk>
2023-03-30 07:59:54 +0530

    embedding a Google map

--- src/components/Contact/Map/Map.jsx
+++ src/components/Contact/Map/Map.jsx
@@ -2,7 +2,7 @@ export const Map = () => {
   return (
     <div className='contacts-map'>
       <iframe
-        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28983.22702991677!2d90.39607920093997!3d24.76450174668472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37564f08e1564b13%3A0xdf7da0a35592c079!2sChorpara%20Bus%20Stop!5e0!3m2!1sen!2sbd!4v1638519781775!5m2!1sen!2sbd'
+        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.287502984853!2d-0.41915848417005475!3d51.691183679665805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876402fabd5cc11%3A0xf1355f57a03a3cba!2s4%20Privet%20Drive!5e0!3m2!1sen!2slk!4v1680142953768!5m2!1sen!2slk'
         width='100%'
         height='450'
         style={{ border: 0 }}
@@ -11,3 +11,4 @@ export const Map = () => {
     </div>
   );
 };
+
