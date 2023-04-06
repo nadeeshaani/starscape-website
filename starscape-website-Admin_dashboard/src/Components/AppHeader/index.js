0a48124860546bd6aee96f41fa30272bfe2c92f6
anuruththan <85021506+anuruththan@users.noreply.github.com>
2023-04-06 23:36:34 +0530

    Add files via upload

---
+++ starscape-website-Admin_dashboard/src/Components/AppHeader/index.js
@@ -0,0 +1,85 @@
+import { BellFilled, MailOutlined } from "@ant-design/icons";
+import { Badge, Drawer, Image, List, Space, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getComments, getOrders } from "../../API";
+import logo from "../assets/logo.png";
+
+function AppHeader() {
+  const [comments, setComments] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [commentsOpen, setCommentsOpen] = useState(false);
+  const [notificationsOpen, setNotificationsOpen] = useState(false);
+
+  useEffect(() => {
+    getComments().then((res) => {
+      setComments(res.comments);
+    });
+    getOrders().then((res) => {
+      setOrders(res.products);
+    });
+  }, []);
+
+  return (
+    <div className="AppHeader">
+      <Image
+        width={40}
+        src={logo}
+      ></Image>
+      <Typography.Title>Starscape Dashboard</Typography.Title>
+      <Space>
+        <Badge count={comments.length} dot>
+          <MailOutlined
+            style={{ fontSize: 24 }}
+            onClick={() => {
+              setCommentsOpen(true);
+            }}
+          />
+        </Badge>
+        <Badge count={orders.length}>
+          <BellFilled
+            style={{ fontSize: 24 }}
+            onClick={() => {
+              setNotificationsOpen(true);
+            }}
+          />
+        </Badge>
+      </Space>
+      <Drawer
+        title="Comments"
+        open={commentsOpen}
+        onClose={() => {
+          setCommentsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={comments}
+          renderItem={(item) => {
+            return <List.Item>{item.body}</List.Item>;
+          }}
+        ></List>
+      </Drawer>
+      <Drawer
+        title="Notifications"
+        open={notificationsOpen}
+        onClose={() => {
+          setNotificationsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={orders}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Typography.Text strong>{item.title}</Typography.Text> has been
+                ordered!
+              </List.Item>
+            );
+          }}
+        ></List>
+      </Drawer>
+    </div>
+  );
+}
+export default AppHeader;
