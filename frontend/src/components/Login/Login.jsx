44e9dafbbe4acded5e55d959f740ab159d655d16
Gimhani Fernando <gimhanif44@gmail.com>
2023-04-07 10:54:19 +0530

    Login connected to front

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,61 +1,99 @@
 import { SocialLogin } from 'components/shared/SocialLogin/SocialLogin';
 import router from 'next/router';
+import { useState } from "react";
+import axios from "axios";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8090/api/v1/auth/authenticate", { email, password });
+      const token = res.data.token;
+      localStorage.setItem("jwtToken", token);
+      // redirect user to the desired page after successful login
+      router.push('/shop');
+    } catch (err) {
+      setError("Invalid email or password");
+    }
+  };
+
   return (
     <>
-      {/* <!-- BEGIN LOGIN --> */}
-      <div className='login'>
-        <div className='wrapper'>
+      <div className="login">
+        <div className="wrapper">
           <div
-            className='login-form js-img'
+            className="login-form js-img"
             style={{ backgroundImage: `url('/assets/img/login-form__bg.png')` }}
           >
-            <form>
+            <form onSubmit={handleSubmit}>
               <h3>log in with</h3>
-              <SocialLogin />
 
-              <div className='box-field'>
+              {/* social login buttons */}
+              <div className="box-field">{/* SocialLogin component here */}</div>
+
+              {/* email or nickname input */}
+              <div className="box-field">
                 <input
-                  type='text'
-                  className='form-control'
-                  placeholder='Enter your email or nickname'
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your email or nickname"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
               </div>
-              <div className='box-field'>
+
+              {/* password input */}
+              <div className="box-field">
                 <input
-                  type='password'
-                  className='form-control'
-                  placeholder='Enter your password'
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
                 />
               </div>
-              <label className='checkbox-box checkbox-box__sm'>
-                <input type='checkbox' />
-                <span className='checkmark'></span>
+
+              {/* remember me checkbox */}
+              <label className="checkbox-box checkbox-box__sm">
+                <input type="checkbox" />
+                <span className="checkmark"></span>
                 Remember me
               </label>
-              <button className='btn' type='submit'>
+
+              {/* submit button */}
+              <button className="btn" type="submit">
                 log in
               </button>
-              <div className='login-form__bottom'>
+
+              {/* error message */}
+              {error && <div className="text-danger">{error}</div>}
+
+              {/* registration and forgot password links */}
+              <div className="login-form__bottom">
                 <span>
-                  No account?{' '}
-                  <a onClick={() => router.push('/registration')}>
+                  No account?{" "}
+                  <a onClick={() => window.location.href = "/registration"}>
                     Register now
                   </a>
                 </span>
-                <a href='#'>Lost your password?</a>
+                <a href="#">Lost your password?</a>
               </div>
             </form>
           </div>
         </div>
         <img
-          className='promo-video__decor js-img'
-          src='/assets/img/promo-video__decor.png'
-          alt=''
+          className="promo-video__decor js-img"
+          src="/assets/img/promo-video__decor.png"
+          alt=""
         />
       </div>
-      {/* <!-- LOGIN EOF   --> */}
     </>
   );
 };
+
