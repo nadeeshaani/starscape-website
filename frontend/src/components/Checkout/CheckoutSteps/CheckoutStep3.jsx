83f8ccc499f21bde2217c4e306fcd98770687836
Nadeeshani <nawarat-se19043@stu.kln.ac.lk>
2023-04-08 15:03:00 +0530

    changes to checkout

--- frontend/src/components/Checkout/CheckoutSteps/CheckoutStep3.jsx
+++ frontend/src/components/Checkout/CheckoutSteps/CheckoutStep3.jsx
@@ -8,22 +8,9 @@ export const CheckoutStep3 = () => {
           <br />
           you for your purchase!
         </h4>
-        <ul className='checkout-purchase__list'>
-          <li>
-            <span>Order number</span>B-125724_75
-          </li>
-          <li>
-            <span>Order status</span>Awaiting payment
-          </li>
-          <li>
-            <span>Reserved for</span>22.09.2020
-          </li>
-          <li>
-            <span>Expected loading date</span>20.09.2020
-          </li>
-        </ul>
+        
         <a href='#' className='checkout-purchase__link'>
-          print a document -
+         Please check your email for order confirmation details.
         </a>
       </div>
       {/* <!-- CHECKOUT STEP TWO EOF -->  */}
